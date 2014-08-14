0f876df79919cc1c3e6f929b6b3725553c18ed04
rootzid <rootzid@gmail.com>
2014-08-14 18:45:47 +0400

    Transaction Builder

---
+++ Trans2Quik.Tests/TransactionBuilderTests.cs
@@ -0,0 +1,50 @@
+﻿namespace Trans2Quik.Tests
+{
+    using System;
+    using Core;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TransactionBuilderTests
+    {
+        private TransactionBuilder tb;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.tb = new TransactionBuilder(1, "ABC001", "TQBR", "SBER");
+        }
+
+        [Test]
+        public void CanCreateNewOrder()
+        {
+            var o = this.tb.NewOrder(Direction.Buy, 73.45M, 2);
+
+            Console.WriteLine(o);
+            Assert.IsNotEmpty(o.ToString());
+            Assert.IsTrue(o.IsLimitOrder.GetValueOrDefault());
+            Assert.IsTrue(this.tb.NextId == 2);
+        }
+
+        [Test]
+        public void CanCreateNewMarketOrder()
+        {
+            var o = this.tb.NewMarketOrder(Direction.Sell, 2);
+
+            Console.WriteLine(o);
+            Assert.IsNotEmpty(o.ToString());
+            Assert.IsFalse(o.IsLimitOrder.GetValueOrDefault());
+            Assert.IsTrue(this.tb.NextId == 2);
+        }
+
+        [Test]
+        public void CanCreateKillOrder()
+        {
+            var o = this.tb.KillOrder(2);
+
+            Console.WriteLine(o);
+            Assert.IsNotEmpty(o.ToString());
+            Assert.IsTrue(o.OrderKey== 2);
+        }
+    }
+}
